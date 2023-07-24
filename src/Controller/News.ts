f5154854a0796dc7946ccba0d702f2ebfaa830fb
thanhphuc <phuchtpc01818@fpt.edu.vn>
2023-07-24 09:58:45 +0700

    update response LoadNews in Vercel

--- src/Controller/News.ts
+++ src/Controller/News.ts
@@ -156,8 +156,8 @@ async function loadNews(req: Request, res: Response) {
     if (!news) {
         res.status(505).json({ message: "Bài viết không tồn tại" });
     } else {
-        res.render('news.ejs', { news: news.content })
-        // res.json(news)
+        // res.render('news.ejs', { news: news.content })
+        res.json(news)
     }
 }
 
