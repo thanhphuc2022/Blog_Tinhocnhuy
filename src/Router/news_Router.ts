b8dfb1cb56ac269f527121a79ab8e5278cb86146
thanhphuc <phuchtpc01818@fpt.edu.vn>
2023-07-31 10:01:57 +0700

    bo midlleware news

--- src/Router/news_Router.ts
+++ src/Router/news_Router.ts
@@ -7,15 +7,15 @@ const news_Router = express();
 
 news_Router.get('/createNews', News.get_CreateNews)
 
-news_Router.post('/createNews', midlleware_file, News.post_CreateNews)
+news_Router.post('/createNews', News.post_CreateNews)
 
-news_Router.post('/updateNews/:id', midlleware_file, News.updateNews)
+news_Router.post('/updateNews/:id', News.updateNews)
 
 news_Router.post('/deleteNews/:id', News.deleteNews)
 
 news_Router.get('/News/:id', News.loadNews)
 
-news_Router.get('/AllNews', middleware, News.loadAllNews)
+news_Router.get('/AllNews', News.loadAllNews)
 
 news_Router.get('/randomNews', News.loadRandomNews)
 
