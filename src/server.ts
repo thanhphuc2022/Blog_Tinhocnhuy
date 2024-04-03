045770656a3da1edab6a2a117927435502bc1737
thanhphuc2022 <hothanhphuc1204@gmail.com>
2024-04-03 16:54:49 +0700

    fix chua chay

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,9 @@ import news_Router from "./Router/news_Router";
 import types_Router from "./Router/types_news_Router";
 import categories_Router from "./Router/categories_Router";
 import tag_Router from "./Router/tag_Router";
-import About_index_Router from "./Router/Display_Router/About_index_Router";
+// import About_index_Router from "./Router/Display_Router/About_index_Router";
 // import Images_About_Index_Router from "./Router/Display_Router/Images_About_Index_Router";
-import About_Router from "./Router/Display_Router/About_Router";
+// import About_Router from "./Router/Display_Router/About_Router";
 import bodyParser from "body-parser";
 import path from "path";
 import { v2 as cloudinary } from 'cloudinary';
@@ -108,11 +108,11 @@ app.use('/api', tag_Router);
 
 //Display
 //about_Index
-app.use('/api', About_index_Router);
+// app.use('/api', About_index_Router);
 //Image_About_Index
 // app.use('/api', Images_About_Index_Router);
 //About
-app.use('/api', About_Router);
+// app.use('/api', About_Router);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
