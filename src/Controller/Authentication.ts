9c079efc15f0636e6f7b935fb295bd9b9c49ac06
thanhphuc2022 <hothanhphuc1204@gmail.com>
2024-08-19 08:18:13 +0700

    update admin phan quyen truy cap

--- src/Controller/Authentication.ts
+++ src/Controller/Authentication.ts
@@ -10,11 +10,15 @@ async function updateRole(req: Request, res: Response) {
     try {
         const username = req.params.username;
         const role = req.body.role;
-        await AccountModel.findOneAndUpdate({ username: username },
-            {
-                role: role
-            })
-        return res.json({ message: "Cập nhật thành công" })
+        if (req.userId == username) {
+            return res.status(400).json("Bạn đang là Admin!!!")
+        } else {
+            await AccountModel.findOneAndUpdate({ username: username },
+                {
+                    role: role
+                })
+            return res.json({ message: "Cập nhật thành công" })
+        }
     } catch (error) {
         return res.json({ message: error })
     }
